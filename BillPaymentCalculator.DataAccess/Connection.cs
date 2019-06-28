158c5807be722ad5acc6ab1f33e660c27e9faf34
Gary Sweet <gary.e.sweet@gmail.com>
2019-06-28 15:56:45 -0700

    Addition of edmx and insBusinessName script

---
+++ BillPaymentCalculator.DataAccess/Connection.cs
@@ -0,0 +1,8 @@
+﻿namespace BillPaymentCalculator.DataAccess
+{
+    public class Connection
+    {
+        string connection = "metadata=res://*/Entities.csdl|res://*/Entities.ssdl|res://*/Entities.msl;provider=System.Data.SqlClient;provider connection string=\"data source=FIELDOFDREAMS;" +
+              "initial catalog=BillPaymentCalculator;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+    }
+}
